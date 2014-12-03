2dc405f3682e53fab6e56fe0491df6bd17611cc2
Aaron Craelius <aaroncraelius@gmail.com>
2014-12-03 18:08:20 -0500

    Tweaks based on Windows work

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 
 (require 'package)
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
+            '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
 	     '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives
@@ -14,7 +14,10 @@
   '(
     ;;;; Globals
     helm
+    smex
     evil
+    evil-surround
+    evil-visualstar
     window-numbering
     editorconfig
     fill-column-indicator
@@ -23,6 +26,9 @@
     projectile
     helm-projectile
     company
+    helm-company
+    idle-highlight-mode
+    minimap
 
     ;;;; Colors & Appearance
     solarized-theme
@@ -51,14 +57,12 @@
 
 (global-set-key (kbd "C-,") 'other-window)
 (global-set-key (kbd "M-/") 'comment-or-uncomment-region)
+(global-set-key (kbd "C-?") 'help-command)
 
 (scroll-bar-mode -1) ;; disables scroll bars
 (setq visible-bell 1) ;; disables audible bells & enables visible bell
-
-;; evil
-
-(require 'evil)
-(evil-mode 1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
 
 ;; helm
 
@@ -66,8 +70,19 @@
 (helm-mode 1)
 (global-set-key (kbd "M-x") 'helm-M-x)
 
+;; smex
+(global-set-key (kbd "M-X") 'smex)
+
 ;; ido
-(global-set-key (kbd "M-a") 'ido-switch-buffer)
+(global-set-key (kbd "M-A") 'ido-switch-buffer)
+(global-set-key (kbd "M-a") 'helm-mini)
+
+;; evil
+
+(require 'evil)
+(require 'evil-visualstar)
+(evil-mode 1)
+(global-evil-surround-mode 1)
 
 ;; window-numbering
 
@@ -85,6 +100,7 @@
 (setq whitespace-style '(face trailing))
 (setq fci-rule-color "#DDD")
 (add-hook 'text-mode-hook 'fci-mode)
+(add-hook 'prog-mode-hook 'fci-mode)
 
 ;; exec-path-from-shell - fix for OS X path problem
 (when (memq window-system '(mac ns))
@@ -97,9 +113,13 @@
 
 ;; projectile
 (projectile-global-mode)
+(setq projectile-indexing-method 'alien)
+(setq projectile-enable-caching t)
 
 ;; company
 (add-hook 'after-init-hook 'global-company-mode)
+
+;; idle-highlight-mode
 
 ;;;; Colors & Appearance
 
@@ -124,8 +144,8 @@
 (define-key sp-keymap (kbd "M-k") 'sp-backward-sexp)
 (define-key sp-keymap (kbd "M-j") 'sp-next-sexp)
 
-(define-key sp-keymap (kbd "M-J") 'sp-down-sexp)
-(define-key sp-keymap (kbd "M-K") 'sp-backward-up-sexp)
+(define-key sp-keymap (kbd "M-S-j") 'sp-down-sexp)
+(define-key sp-keymap (kbd "M-S-k") 'sp-backward-up-sexp)
 
 (define-key sp-keymap (kbd "M-l") 'sp-forward-sexp)
 (define-key sp-keymap (kbd "M-h") 'sp-previous-sexp)
@@ -150,8 +170,8 @@
 
 (define-key sp-keymap (kbd "C-M-l") 'sp-forward-slurp-sexp)
 (define-key sp-keymap (kbd "C-M-h") 'sp-forward-barf-sexp)
-(define-key sp-keymap (kbd "C-M-H") 'sp-backward-slurp-sexp)
-(define-key sp-keymap (kbd "C-M-L") 'sp-backward-barf-sexp)
+(define-key sp-keymap (kbd "C-M-S-h") 'sp-backward-slurp-sexp)
+(define-key sp-keymap (kbd "C-M-S-l") 'sp-backward-barf-sexp)
 
 ;; (define-key sp-keymap (kbd "M-D") 'sp-splice-sexp)
 ;; (define-key sp-keymap (kbd "C-M-<delete>") 'sp-splice-sexp-killing-forward)
@@ -184,8 +204,29 @@
 
 ;; cider
 
+(setq cider-prompt-save-file-on-load nil)
+
+;; clj-refactor
+
+(add-hook 'cider-mode-hook (lambda ()
+			     (clj-refactor-mode 1)
+			     (cljr-add-keybindings-with-prefix "C-x C-r")))
 
 ;;;; Platform-specific stuff
+
+;; for tmux
+(add-hook 'after-make-frame-functions
+  (lambda ()
+    (when (not window-system)
+      ;;(normal-erase-is-backspace-mode 1)
+      (global-set-key (kbd "C-h") 'delete-backward-char))))
+
+;; for Windows
+(when (or (eq system-type 'windows-nt) (eq system-type 'msdos))
+   (setenv "PATH" (concat "C:\\gnuwin\\bin;" (getenv "PATH")))
+   (setq find-program "C:\gnuwin\\bin\\find.exe"
+	 grep-program "C:\\gnuwin\\bin\\grep.exe"))
+
 
 ;;;;;;;; added by Custom
 (custom-set-variables
@@ -197,7 +238,7 @@
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(flx-ido-mode t)
- '(magit-diff-use-overlays nil))
+ '(magit-use-overlays nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
