14bea3517e84d33d11164e177bcb6dd049fa8757
Aaron Craelius <aaroncraelius@gmail.com>
2014-12-12 00:05:34 -0500

    Changes from OS X and terminal.

--- init.el
+++ init.el
@@ -128,7 +128,7 @@
       (set-face-attribute 'default nil :font "Consolas-14")
     (set-face-attribute 'default nil :font "Inconsolata-15")))
 
-(load-theme 'solarized-light)
+(load-theme 'solarized-light t)
 
 ;;;; Lisp
 
@@ -215,11 +215,19 @@
 ;;;; Platform-specific stuff
 
 ;; for tmux
-(add-hook 'after-make-frame-functions
-  (lambda ()
-    (when (not window-system)
-      ;;(normal-erase-is-backspace-mode 1)
-      (global-set-key (kbd "C-h") 'delete-backward-char))))
+;; (add-hook 'after-make-frame-functions
+;;   (lambda ()
+;;     (when (not window-system)
+;;       ;;(normal-erase-is-backspace-mode 1)
+;;       ;(global-set-key (kbd "C-h") 'delete-backward-char)
+;;       )))
+
+;; for OSX
+(when (eq system-type 'darwin) ;; mac specific settings
+   (setq mac-option-modifier 'alt)
+   (setq mac-command-modifier 'meta)
+   (global-set-key [kp-delete] 'delete-char) ;; sets fn-delete to be right-delete
+     )
 
 ;; for Windows
 (when (or (eq system-type 'windows-nt) (eq system-type 'msdos))
@@ -236,7 +244,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+    ("e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(flx-ido-mode t)
  '(magit-use-overlays nil))
 (custom-set-faces
